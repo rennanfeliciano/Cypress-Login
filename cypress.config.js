aac92cc84e9a9223f09c43b54805b2046f0c5dcd
Rennan Feliciano <rennan.feliciano@gmail.com>
2024-02-28 15:52:38 -0300

    Atualização

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: '7qi167',
   e2e: {
     baseUrl: 'https://automationpratice.com.br/',
     defaultCommandTimeout: 4000,
